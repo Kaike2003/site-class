f1a9f9a7e668f0bab60b88a9c36a8bbc6b67a48b
kaike2003 <kaikebartolomeu2003@gmail.com>
2025-01-20 23:51:11 +0000

    003

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  productionBrowserSourceMaps: true,
+  images: {
+    domains: ["google.com", "maps.google.com"],
+  },
+};
 
 export default nextConfig;
